b928448815e75f656b7a7abb3fe3ea515ca6ebc2
dnwls7738 <cjsdnwls7738@gmail.com>
2023-09-11 14:31:46 +0900

    라우팅및파이어베이스추가

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./components/layout";
+import HOME from "./routes/home";
+import Profile from "./routes/profile";
+import Login from "./routes/login";
+import CreactAccount from "./routes/creact_account";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import { useEffect, useState } from "react";
+import Loading from "./components/loading";
+import { auth } from "./firebase";
 
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Layout />,
+		children: [
+			{
+				path: "",
+				element: <HOME />,
+			},
+			{
+				path: "profile",
+				element: <Profile />,
+			},
+		],
+	},
+	{
+		path: "/login",
+		element: <Login />,
+	},
+	{
+		path: "/create_account",
+		element: <CreactAccount />,
+	},
+]);
+
+const GlobalStyles = createGlobalStyle`
+${reset};
+*{
+	box-sizing: border-box;
+}
+body{
+	background-color: black;
+	color: white;	
+}
+`;
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [isLoading, setIsLoading] = useState(true);
+	const init = async () => {
+		await auth.authStateReady();
+		setIsLoading(false);
+	};
+	useEffect(() => {
+		init();
+	}, []);
+	return (
+		<>
+			<GlobalStyles />
+			{isLoading ? <Loading /> : <RouterProvider router={router} />}
+		</>
+	);
 }
 
 export default App;
---
+++ src/routes/creact_account.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Creact_account() {
+	return (
+		<div>
+			<h1>Creact_account</h1>
+		</div>
+	);
+}
+
+export default Creact_account;
