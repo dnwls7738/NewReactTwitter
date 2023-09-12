9f8d7bc0639ece37d671b556d9592ff4d6c93fbe
dnwls7738 <cjsdnwls7738@gmail.com>
2023-09-12 17:02:55 +0900

    firestore연결

--- src/components/PostTweetForm.jsx
+++ src/components/PostTweetForm.jsx
@@ -1,5 +1,7 @@
+import { addDoc, collection } from "firebase/firestore";
 import React, { useState } from "react";
 import styled from "styled-components";
+import { auth, db } from "../firebase";
 
 const Form = styled.form`
 	display: flex;
@@ -66,8 +68,28 @@ function PostTweetForm() {
 			setFile(files[0]);
 		}
 	};
+	const onSubmit = async (e) => {
+		e.preventDefault();
+		const user = auth.currentUser;
+		if (!user || isLoading || tweet === "" || tweet.length > 500)
+			return;
+
+		try {
+			setIsLoading(true);
+			await addDoc(collection(db, "tweets"), {
+				tweet,
+				createdAt: Date.now(),
+				username: user.displayName || "Anonymous",
+				userId: user.uid,
+			});
+		} catch (e) {
+			console.log(e);
+		} finally {
+			setIsLoading(false);
+		}
+	};
 	return (
-		<Form>
+		<Form onSubmit={onSubmit}>
 			<TextArea
 				rows={5}
 				maxLength={500}
